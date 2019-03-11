7f7f3c90e358a9576d60142d833dbc5e71a7d4b3
laurent6 <lau.lau88@hotmail.fr>
2019-03-11 09:13:27 +0100

    add arg option in tests

--- babeld-master/Tests/test/main.py
+++ babeld-master/Tests/test/main.py
@@ -1,11 +1,31 @@
 import babel
 import perf
 import TestRoute
+import sys, getopt
 def main():
     #todo
     #babel.startB("ens3")
     babel.test_downBattery()
     perf.startPerf(25,"ens3")
     #TestRoute.test_is_link()
+
+def usage():
+    print("usage: \n\t -b : to test battery criteria"+
+          "\n\t -p <nbPerf>: performance test nb_times"+
+          "\n\t -h : display help")
 if __name__ == "__main__":
-    main()
+    #main()
+    try:
+        opts,args= getopt.getopt(sys.argv[1:], "hbp:",["nbperf="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(0)
+    for opt,arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit(0)
+        elif opt in ("-p","--nbPerf"):
+            perf.startPerf(int(arg), "ens3")
+        elif opt == '-b':
+            babel.test_downBattery()
+
