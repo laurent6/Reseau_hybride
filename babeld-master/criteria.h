12dd96d87f6208fc3a6c8c1c4e57a297dd58a925
laurent6 <lau.lau88@hotmail.fr>
2019-03-20 16:23:22 +0100

    nothing

--- babeld-master/criteria.h
+++ babeld-master/criteria.h
@@ -1,22 +1,9 @@
 
-
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> parent of a4610bd... delay ok but no test
-
-
 #define MESSAGE_BATTERY 40
 #define LENGTH_ALL_CRITERIA 3
 #define LENGTH_CRITERIA_BATTERY 1
 
-
-
 void push_criteria(struct buffered *buf);
 void update_metric_battery_criteria(int * metric);
 int is_battery_critical(int b);
-<<<<<<< HEAD
-=======
 void update_delay_neighbour_criteria(struct neighbour *neigh);
->>>>>>> parent of a4610bd... delay ok but no test
