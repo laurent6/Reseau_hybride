a7a3afc32c3ab5ec2a9a2e27851945cc21996494
laurent6 <lau.lau88@hotmail.fr>
2019-03-20 16:23:57 +0100

    nothing

--- babeld-master/criteria.h
+++ babeld-master/criteria.h
@@ -3,6 +3,8 @@
 #define LENGTH_ALL_CRITERIA 3
 #define LENGTH_CRITERIA_BATTERY 1
 
+
+
 void push_criteria(struct buffered *buf);
 void update_metric_battery_criteria(int * metric);
 int is_battery_critical(int b);
